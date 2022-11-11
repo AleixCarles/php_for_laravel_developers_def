25771b928f2310f7f6e1a7707aaf5cf7c92f9b65
Aleix Carles <aleixcarles@iesebre.com>
2022-11-11 17:48:51 +0100

    video 10 acavat

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<x-layout>
+    <div class="py-10">
+        <header>
+            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <h1 class="text-3xl font-bold leading-tight tracking-tight text-gray-900">ABOUT</h1>
+            </div>
+        </header>
+        <main>
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-10">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet autem eveniet expedita facilis illum laboriosam modi nobis provident, quae quidem ratione reprehenderit sapiente, sequi sint tempora tempore voluptas voluptatem?
+            </div>
+
+        </main>
+    </div>
+
+</x-layout>
