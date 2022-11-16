8855096cb52fd30aefc429d00780263ae8640015
Aleix Carles <aleixcarles@iesebre.com>
2022-11-16 17:49:17 +0100

    Refactoritzar la funció anonima que tenia a routes/web.php a un controlador

--- routes/web.php
+++ routes/web.php
@@ -1,41 +1,10 @@
 <?php
 
-use App\Models\TaskP;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/', function () {
-//    $task1 = TaskP::create([
-//        'title' => 'Tasca1',
-//        'description' => 'lorem bla bla',
-//        'completed' => 0
-//    ]);
-//    $task2 = TaskP::create([
-//        'title' => 'Tasca2',
-//        'description' => 'lorem bla bla',
-//        'completed' => 1
-//    ]);
-//    $task3 = TaskP::create([
-//        'title' => 'Tasca3',
-//        'description' => 'lorem bla bla',
-//        'completed' => 0
-//    ]);
-//    $tasks = [
-//        $task1,
-//        $task2,
-//        $task3
-//    ];
-
-    return view('tasks', [
-        'tasks' => TaskP::all()
-    ]);
-});
-
-Route::get('/tasks', function () {
-    return view('tasks', [
-        'tasks' => []
-    ]);
-});
+Route::get('/', [TaskController::class,'index']) ;
+Route::get('/tasks', [TaskController::class,'index']);
 
 Route::get('/users', function () {
     return view('users');
